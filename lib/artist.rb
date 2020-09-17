3e2698a15c9c1e5f6bacb9390a3882cd254cbba7
Adi Skop <adiskop@gmail.com>
2020-09-17 09:24:40 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,51 +1,50 @@
-
 class Artist
-attr_accessor :name
-attr_reader :songs
+  extend Concerns::Findable
 
-extend Concerns::Findable
+  attr_accessor :name, :songs, :genre
 
-@@all=[]
+  @@all = []
 
-def initialize(name)
-@name=name
-save
-@songs=[]
-end
-
-def self.all
-@@all
-end
-
-def self.destroy_all
-@@all.clear
-end
-
-def save
-@@all<<self
-end
-
-def self.create(name)
-newArtist=self.new(name)
-
-end
-
-def add_song(song)
-  song.artist =self unless song.artist
-  @songs<<song unless songs.include?(song)
-end
-
-def genres
-    songs.collect(&:genre).uniq
+  def initialize(name)
+    @name = name
+    @songs = []
+    save
   end
 
+  def add_song(song)
+    if song.artist == nil
+      song.artist = self
+    else
+      nil
+    end
+    unless @songs.include?(song)
+    @songs << song
+    end
+    song
+  end
 
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.create(name)
+    Artist.new(name)
+  end
+
+  def songs
+    @songs
+  end
+
+  def genres
+    genre_array = @songs.collect {|song| song.genre}
+    genre_array.uniq
+  end
 end
-
-
-
-
-
-#learn spec/004_songs_and_artists_spec.rb
-#learn spec/005_songs_and_genres_spec.rb
-#learn spec/006_artists_and_genres_spec.rb
